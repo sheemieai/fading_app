94ac0f293dd2df8b8a9ba3310ccb228fa4e2a2ce
Jahsheem Charles <jahsheemc@gmail.com>
2024-10-02 19:01:18 -0400

    Added a fading in and out image and a loading img

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,10 +22,48 @@ class FadingTextAnimation extends StatefulWidget {
 
 class _FadingTextAnimationState extends State<FadingTextAnimation> {
   bool _isVisible = true;
+  final Random random = Random();
+  int randomImgNumber = 0;
+  int lastImgNumber = 0;
+  int loadingImgNumber = 0;
+  double numberOfTurns = 0.0;
+  final String loadingSquareImg = "lib/img/square.png";
+  final List<Color> loadingColorList = [Colors.green, Colors.red];
+  final List<String> ghostImgList = [
+    "lib/img/ghost-cool.png", "lib/img/ghost-dap.png", "lib/img/ghost-go.png",
+    "lib/img/ghost-mad.png", "lib/img/ghost-peace.png", "lib/img/ghost-thumbs.png"
+  ];
+  final List<String> ghostEmotionString = [
+    "I am so cool!", "I am dapping, yo!", "Let's Go!!",
+    "I am so mad, GRRRR!!", "Peace out Homie!!", "Great Job!!"
+  ];
+
 
   void toggleVisibility() {
     setState(() {
+      loadingImgNumber = loadingImgNumber == 0 ? 1 : 0;
+
+      if (!_isVisible) {
+        getRandomNumber();
+      }
+
       _isVisible = !_isVisible;
+      rotateTheImg();
+      lastImgNumber = randomImgNumber;
+    });
+  }
+
+  void getRandomNumber() {
+    setState(() {
+      while (randomImgNumber == lastImgNumber) {
+        randomImgNumber = random.nextInt(6);
+      }
+    });
+  }
+
+  void rotateTheImg() {
+    setState(() {
+      numberOfTurns++;
     });
   }
 
@@ -31,21 +71,43 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Fading Text Animation'),
+        title: const Text('Fading Text Animation'),
       ),
       body: Center(
-        child: AnimatedOpacity(
-          opacity: _isVisible ? 1.0 : 0.0,
-          duration: Duration(seconds: 1),
-          child: Text(
-            'Hello, Flutter!',
-            style: TextStyle(fontSize: 24),
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedOpacity(
+              opacity: _isVisible ? 1.0 : 0.0,
+              duration: const Duration(seconds: 1),
+              child: Text(
+                ghostEmotionString[randomImgNumber],
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+            AnimatedOpacity(
+              opacity: _isVisible ? 1.0 : 0.0,
+              duration: const Duration(seconds: 1),
+              child: Image.asset(
+                ghostImgList[randomImgNumber],
+              )
+            ),
+            AnimatedRotation(
+              turns: numberOfTurns,
+              duration: const Duration(seconds: 1),
+              child: Image.asset(
+                loadingSquareImg,
+                width: 40.0,
+                height: 40.0,
+                color: loadingColorList[loadingImgNumber],
+              ),
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: toggleVisibility,
-        child: Icon(Icons.play_arrow),
+        child: const Icon(Icons.play_arrow),
       ),
     );
   }
